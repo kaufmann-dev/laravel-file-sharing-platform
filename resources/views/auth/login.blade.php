6a5121e46c2e265595041b6ff6331f15f9a42bee
David Kaufmann <david@kaufmann.dev>
2022-12-06 12:09:56 +0100

    Login Register Link

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,54 +6,50 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+            crossorigin="anonymous"></script>
     <title>Login</title>
 </head>
 <body>
 
 @extends('app')
 @section('content')
-    <main class="login-form">
-        <div class="cotainer border" style="height: 1vh">
-            <div class="row justify-content-center align-items-center">
-                <div class="col-md-4">
-                    <div class="card">
-                        <h3 class="card-header text-center">Login</h3>
-                        <div class="card-body">
-                            <form method="POST" action="{{ route('login.custom') }}">
-                                @csrf
-                                <div class="form-group mb-3">
-                                    <input type="text" placeholder="Email" id="email" class="form-control" name="email" required
-                                           autofocus>
-                                    @if ($errors->has('email'))
-                                        <span class="text-danger">{{ $errors->first('email') }}</span>
-                                    @endif
-                                </div>
-                                <div class="form-group mb-3">
-                                    <input type="password" placeholder="Password" id="password" class="form-control" name="password" required>
-                                    @if ($errors->has('password'))
-                                        <span class="text-danger">{{ $errors->first('password') }}</span>
-                                    @endif
-                                </div>
-                                <div class="form-group mb-3">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="remember"> Remember Me
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="d-grid mx-auto">
-                                    <button type="submit" class="btn btn-dark btn-block">Sign in</button>
-                                </div>
-                            </form>
-                        </div>
+    <div class="container w-25 d-flex justify-content-center flex-column vh-100">
+        <h1 class="display-1 text-center">Login</h1>
+
+        <div class="row">
+            <div class="col-12 mt-3">
+                <form method="POST" action="{{ route('login.custom') }}">
+                    @csrf
+                    <div class="form-group mb-3">
+                        <input type="text" placeholder="Email" id="email" class="form-control" name="email" required
+                               autofocus>
+                        @if ($errors->has('email'))
+                            <span class="text-danger">{{ $errors->first('email') }}</span>
+                        @endif
                     </div>
-                </div>
+                    <div class="form-group mb-3">
+                        <input type="password" placeholder="Password" id="password" class="form-control" name="password"
+                               required>
+                        @if ($errors->has('password'))
+                            <span class="text-danger">{{ $errors->first('password') }}</span>
+                        @endif
+                    </div>
+                    <div id="register" class="text-muted mb-3">
+                        Noch kein Account?
+                        <a href="{{ route('register-user') }}" class="text-decoration-none">Registrieren</a>
+                    </div>
+                    <div class="d-grid mx-auto">
+                        <button type="submit" class="btn btn-dark btn-block">Sign in</button>
+                    </div>
+                </form>
             </div>
         </div>
-    </main>
+    </div>
 @endsection
 
 </body>
