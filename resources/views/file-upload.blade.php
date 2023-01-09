e31aedbc88f43493d18315851287a9c67b9148df
David Kaufmann <david@kaufmann.dev>
2023-01-09 14:50:22 +0100

    FileUploadUpdate

---
+++ resources/views/file-upload.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <title>Laravel File Upload</title>
+    <style>
+        .container {
+            max-width: 500px;
+        }
+        dl, ol, ul {
+            margin: 0;
+            padding: 0;
+            list-style: none;
+        }
+    </style>
+</head>
+<body>
+<div class="container mt-5">
+    <form action="{{ route('fileUpload') }}" method="post" enctype="multipart/form-data">
+        <h3 class="text-center mb-5">Upload File in Laravel</h3>
+        @csrf
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                <strong>{{ $message }}</strong>
+            </div>
+        @endif
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <div class="custom-file">
+            <input type="file" name="file" class="form-control" id="formFile">
+        </div>
+        @if($message = Session::get("success"))
+            <a href="{{ route("dashboard") }}" class="btn btn-secondary mt-4">
+                Go back to dashboard
+            </a>
+        @else
+            <button type="submit" name="submit" class="btn btn-primary mt-4">
+                Upload Files
+            </button>
+        @endif
+    </form>
+</div>
+</body>
+</html>
