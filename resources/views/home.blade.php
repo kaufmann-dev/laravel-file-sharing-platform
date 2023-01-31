73ffc65b77f4b31ea4738834a9c6fd0419e9e34c
David Kaufmann <david@kaufmann.dev>
2023-01-31 08:49:04 +0100

    sort table only file names fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -56,25 +56,17 @@
                     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
                     table = document.getElementById("dataTable");
                     switching = true;
-                    // Set the sorting direction to ascending:
                     dir = "asc";
-                    /* Make a loop that will continue until
-                    no switching has been done: */
                     while (switching) {
-                        // Start by saying: no switching is done:
                         switching = false;
                         rows = table.rows;
-                        /* Loop through all table rows (except the
-                        first, which contains table headers): */
+                        /* Loop through all table rows starting at 2nd row */
                         for (i = 1; i < (rows.length - 1); i++) {
                             // Start by saying there should be no switching:
                             shouldSwitch = false;
-                            /* Get the two elements you want to compare,
-                            one from current row and one from the next: */
                             x = rows[i].getElementsByTagName("TD")[n];
                             y = rows[i + 1].getElementsByTagName("TD")[n];
-                            /* Check if the two rows should switch place,
-                            based on the direction, asc or desc: */
+
                             if (dir == "asc") {
                                 if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
                                     // If so, mark as a switch and break the loop:
@@ -97,8 +89,6 @@
                             // Each time a switch is done, increase this count by 1:
                             switchcount ++;
                         } else {
-                            /* If no switching has been done AND the direction is "asc",
-                            set the direction to "desc" and run the while loop again. */
                             if (switchcount == 0 && dir == "asc") {
                                 dir = "desc";
                                 switching = true;
