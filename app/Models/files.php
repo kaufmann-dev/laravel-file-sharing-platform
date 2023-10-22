6977e2921e96ba88929077f89ce2a2897c5e94d5
David Kaufmann <david@kaufmann.dev>
2023-10-22 16:31:15 +0100

    fix: update app/Models/

--- app/Models/files.php
+++ app/Models/files.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class files extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'filename',
+        'created',
+        'edited',
+        'file_id',
+        'folder_id'
+    ];
 }
--- app/Models/user_has_data.php
+++ app/Models/user_has_data.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class user_has_data extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'data_id',
+        'relationship'
+    ];
 }
