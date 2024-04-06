843fde1ca12307d8aff7931444ac8ac38083d885
David Kaufmann <david@kaufmann.dev>
2024-04-06 11:01:31 +0100

    fix: update resources/views/

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
 <body>
 <nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
     <div class="container">
-        <a class="navbar-brand mr-auto" href="#">PositronX</a>
+        <a class="navbar-brand mr-auto" href="#">FShare</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -31,5 +31,7 @@
     </div>
 </nav>
 @yield('content')
+
+<button type="button" onclick="window.location='http://127.0.0.1:8000/fupload'">Lade Files hoch</button>
 </body>
 </html>
