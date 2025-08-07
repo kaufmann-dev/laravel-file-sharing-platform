72551afff094da197720d9408038edfa38eb423d
David Kaufmann <david@kaufmann.dev>
2025-08-07 10:44:42 +0100

    fix: update app/Http/Controllers/

--- app/Models/File.php
+++ app/Models/File.php
@@ -2,6 +2,7 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class File extends Model
 {
@@ -9,9 +10,10 @@ class File extends Model
     protected $fillable = [
         'name',
         'user_id',
-        'file_path'
+        'file_path',
+        'file_size'
     ];
-    
+
     public function user(){
         return $this->belongsTo(User::class);
     }
